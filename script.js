723ab87e454330e701055fad18db1b623243d886
apoiosustentabilidade-tech <apoiosustentabilidade@gmail.com>
2026-09-07 18:16:45 -0300

    Padroniza campo de telefone

--- script.js
+++ script.js
@@ -32,7 +32,38 @@ cnpjInput?.addEventListener('input', (event) => {
 
   event.target.setCustomValidity('');
 });
+const telefoneInput = document.getElementById('telefone');
 
+telefoneInput?.addEventListener('input', (event) => {
+  let value = event.target.value
+    .replace(/\D/g, '')
+    .slice(0, 11);
+
+  if (value.length > 10) {
+    value = value.replace(
+      /^(\d{2})(\d{5})(\d{4})$/,
+      '($1) $2-$3'
+    );
+  } else if (value.length > 6) {
+    value = value.replace(
+      /^(\d{2})(\d{4})(\d{0,4})$/,
+      '($1) $2-$3'
+    );
+  } else if (value.length > 2) {
+    value = value.replace(
+      /^(\d{2})(\d{0,5})$/,
+      '($1) $2'
+    );
+  } else if (value.length > 0) {
+    value = value.replace(
+      /^(\d{0,2})$/,
+      '($1'
+    );
+  }
+
+  event.target.value = value;
+  event.target.setCustomValidity('');
+});
 
 /* =========================
    ENVIO DO FORMULÁRIO
